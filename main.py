08978365ab40e79b548a94ab6aa1ead9ea9064b8
RashmiICT <96565@fhss.sjp.ac.lk>
2021-06-07 19:44:14 +0530

    first comment

---
+++ main.py
@@ -0,0 +1,7 @@
+from stack import stack
+
+myStack = stack(5)
+
+myStack. push(10)
+myStack. push(20)
+myStack. push(30)
---
+++ stack.py
@@ -0,0 +1,9 @@
+"""
+1. Create a class Node with instance variables data and next.
+2. Create a class Stack with instance variable head.
+3. The variable head points to the first element in the linked list.
+4. Define methods push and pop inside the class Stack.
+5. The method push adds a node at the front of the linked list.
+6. The method pop returns the data of the node at the front of the linked list and removes the node. It returns None if there are no nodes.
+7. Create an instance of Stack and present a menu to the user to perform operations on the stack.
+"""
