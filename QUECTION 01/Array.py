1746f610e54487fbb4a596d85f0c163232bc18bc
RashmiICT <96565@fhss.sjp.ac.lk>
2021-06-12 11:01:08 +0530

    push method

--- QUECTION 01/Array.py
+++ QUECTION 01/Array.py
@@ -5,3 +5,10 @@ class stack:
     def __init__(self, limit=10):
         self.limit = limit
         self.stack = np.array([])
+
+    def push(self, data):
+        if len(self.stack) <= self.limit:
+            self.stack = np.append(self.stack, data)
+            print("PUSH  ELEMENT IS >>", data)
+
+
