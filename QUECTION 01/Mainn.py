1e49a64d5e02077350a264f1b9f5389499e26c7c
RashmiICT <96565@fhss.sjp.ac.lk>
2021-06-12 17:10:25 +0530

    COMMENT

--- QUECTION 01/Mainn.py
+++ QUECTION 01/Mainn.py
@@ -2,14 +2,21 @@ from Array import stack
 
 mystack =stack(5)
 
-mystack.isEmpty()
 
-#
+print("===================================================")
+mystack.isEmpty()
+print("===================================================\n")
+
+
+print("~~~~~~~~~~~~~~~~~~PUSH ELEMENT~~~~~~~~~~~~~~~~~~~~~~~\n")
 mystack.push(10)
 mystack.push(20)
 mystack.push(30)
 mystack.push(40)
+print("===================================================\n")
 
+
+print("~~~~~~~~~~~~~~~~~~TOP ELEMENT~~~~~~~~~~~~~~~~~~~~~~~\n")
 mystack.top()
 
 x = mystack.size()
